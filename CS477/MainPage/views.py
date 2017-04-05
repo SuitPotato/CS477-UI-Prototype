e91c6396827429f359a4f79e7713243a2867f4b6
スーツ芋 <Erekyth@yahoo.com>
2017-04-04 21:36:28 -0700

    Working prototype

--- CS477/MainPage/views.py
+++ CS477/MainPage/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+def index(request):
+	return render(request, 'MainPage/index.html')
